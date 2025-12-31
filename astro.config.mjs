3a7972ed770bb8bd89df7449058084ecb9451bda
Julian Mican <julian.mican@gmail.com>
2025-12-31 11:31:22 -0500

    add: improvements to seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import sitemap from "@astrojs/sitemap";
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://julianjjo.github.io', // Replace with your actual domain
+  integrations: [tailwind(), sitemap()],
   output: "server",
   adapter: node({
     mode: "middleware"
