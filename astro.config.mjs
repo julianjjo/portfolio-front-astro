b6c83ca6e9310a418ab09ab4864af6232533adab
Julian Mican <julian.mican@gmail.com>
2025-12-31 11:41:57 -0500

    fix: title seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+<<<<<<< Updated upstream
   site: 'https://julianjjo.github.io', // Replace with your actual domain
+=======
+  site: 'https://julianjjo.github.io',
+>>>>>>> Stashed changes
   integrations: [tailwind(), sitemap()],
   output: "server",
   adapter: node({
